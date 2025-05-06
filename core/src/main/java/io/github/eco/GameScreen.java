8218915b1311f9ad38735542f152624668a9bea5
Sevennightoff <103973044+Sevennightoff@users.noreply.github.com>
2025-05-05 21:19:19 -0300

    Update GameScreen.java

--- core/src/main/java/io/github/eco/GameScreen.java
+++ core/src/main/java/io/github/eco/GameScreen.java
@@ -2,7 +2,7 @@ package io.github.eco;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.Array;
 
@@ -20,36 +20,30 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
-        player = new Player(100, Gdx.graphics.getHeight() / 2f, 20);
+        shapeRenderer = new ShapeRenderer();
+        player = new Player(100, Gdx.graphics.getHeight() / 2, 20);
         obstacles = new Array<>();
         collectibles = new Array<>();
-        shapeRenderer = new ShapeRenderer();
+
+        for (int i = 0; i < 5; i++) {
+            obstacles.add(new Obstacle(400 + i * 300, 0, 50, 200 + i * 20));
+            collectibles.add(new Collectible(600 + i * 300));
+        }
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         player.update(delta);
-
-        for (Obstacle obstacle : obstacles) {
-            obstacle.update(delta);
-        }
-
-        for (Collectible collectible : collectibles) {
-            collectible.update(delta);
-        }
-
-        SpriteBatch batch = game.getBatch();
-        batch.begin();
-        player.draw(batch);
-        for (Obstacle obstacle : obstacles) {
-            obstacle.draw(batch);
-        }
-        batch.end();
+        for (Obstacle obstacle : obstacles) obstacle.update(delta);
+        for (Collectible collectible : collectibles) collectible.update(delta);
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        for (Collectible collectible : collectibles) {
-            collectible.draw(shapeRenderer);
-        }
+        player.draw(shapeRenderer);
+        for (Obstacle obstacle : obstacles) obstacle.draw(shapeRenderer);
+        for (Collectible collectible : collectibles) collectible.draw(shapeRenderer);
         shapeRenderer.end();
     }
 
@@ -60,7 +54,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-        player.dispose();
         shapeRenderer.dispose();
     }
 }
