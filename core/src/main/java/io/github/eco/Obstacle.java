1dbcbfaa5585d1ff5651b19efdfb1d75441e8664
Sevennightoff <103973044+Sevennightoff@users.noreply.github.com>
2025-05-05 21:19:56 -0300

    Update Obstacle.java

--- core/src/main/java/io/github/eco/Obstacle.java
+++ core/src/main/java/io/github/eco/Obstacle.java
@@ -1,33 +1,27 @@
 package io.github.eco;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Obstacle {
     public float x, y, width, height;
-    private Texture obstacleTexture;
 
     public Obstacle(float x, float y, float width, float height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.obstacleTexture = new Texture("obstacle.png"); // Certifique-se de ter esta imagem
     }
 
     public void update(float delta) {
         x -= 200 * delta;
     }
 
-    public void draw(SpriteBatch batch) {
-        batch.draw(obstacleTexture, x, y, width, height);
+    public void draw(ShapeRenderer sr) {
+        sr.setColor(0, 1, 0, 1); // Verde
+        sr.rect(x, y, width, height);
     }
 
     public boolean isOffScreen() {
         return x + width < 0;
-    }
-
-    public void dispose() {
-        obstacleTexture.dispose();
     }
 }
